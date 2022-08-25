9647d5b9683adc67372073bccd9608dc6a153b61
RakulAgn <rakul0agn@gmail.com>
2022-08-25 21:45:05 +0530

    ..

--- src/products/products.service.ts
+++ src/products/products.service.ts
@@ -2,6 +2,7 @@ import { Injectable } from "@nestjs/common";
 import { Product } from "./product.model";
 import * as fs from 'fs';
 
+
 @Injectable()
 export class ProductsService {
 
@@ -15,10 +16,17 @@ export class ProductsService {
         const newProduct = new Product(prodId, title, desc, price);
         this.products.push(newProduct);
 
-        JSON.stringify(newProduct,  function(err, output) {
-            fs.writeFileSync(__dirname+'/product.csv',output)
-        })
-
+        const CSV_DATA = `PRODUCT_ID,TITLE,DESCRIPTION,PRICE
+        ${prodId},${title},${desc},${price}`
+        
+        fs.writeFile(__dirname+'/product.csv',CSV_DATA,{encoding: "utf8", flag: "a" }, (err) => {
+            if(err) {
+                console.log(err);
+            } else {
+                console.log("Data Saved");
+                
+            }
+        });
         return prodId;
     }
 
