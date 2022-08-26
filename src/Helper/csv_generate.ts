bd24c1460848f86001bc55e408478a791b676659
RakulAgn <rakul0agn@gmail.com>
2022-08-26 15:47:11 +0530

    Condition Modified

--- src/Helper/csv_generate.ts
+++ src/Helper/csv_generate.ts
@@ -1,9 +1,10 @@
 import * as fs from 'fs';
 
-export const GenerateCsv = (prodId:string, title:string, desc:string, price:number) => {
+export const GenerateCsv = (prodId: string, title: string, desc: string, price: number) => {
 
+    const fileExists = fs.existsSync('product.csv');
 
-    if (fs.existsSync('product.csv') === false) {
+    if (!fileExists) {
         const CSV_DATA = `PRODUCT_ID,TITLE,DESCRIPTION,PRICE\n${prodId},${title},${desc},${price}`
 
         return fs.writeFile('product.csv', CSV_DATA, { encoding: "utf8", flag: "a+" }, (err) => {
@@ -13,38 +14,23 @@ export const GenerateCsv = (prodId:string, title:string, desc:string, price:numb
                 console.log("File Create Successfully Data Written");
             }
         });
-        
-    }   else {
+
+    } else {
 
         fs.readFile('product.csv', 'utf-8', function (err, data) {
 
             var firstColumn = data.includes('PRODUCT_ID,TITLE,DESCRIPTION,PRICE');
 
-            switch (firstColumn) {
+            if (firstColumn) {
+                const CSV_DATA = `\n${prodId},${title},${desc},${price}`
 
-                case false: {
-                    const CSV_DATA = `PRODUCT_ID,TITLE,DESCRIPTION,PRICE\n${prodId},${title},${desc},${price}`
-
-                    return fs.writeFile('product.csv', CSV_DATA, { encoding: "utf8", flag: "a+" }, (err) => {
-                        if (err) {
-                            console.log(err);
-                        } else {
-                            console.log("Inserted Data Successfully");
-                        }
-                    });
-                }
-                case true: {
-                    const CSV_DATA = `\n${prodId},${title},${desc},${price}`
-
-                    return fs.writeFile('product.csv', CSV_DATA, { encoding: "utf8", flag: "a+" }, (err) => {
-                        if (err) {
-                            console.log(err);
-                        } else {
-                            console.log("Row Inserted Successfully");
-
-                        }
-                    });
-                }
+                return fs.writeFile('product.csv', CSV_DATA, { encoding: "utf8", flag: "a+" }, (err) => {
+                    if (err) {
+                        console.log(err);
+                    } else {
+                        console.log("Row Inserted Successfully");
+                    }
+                });
             }
 
         })
