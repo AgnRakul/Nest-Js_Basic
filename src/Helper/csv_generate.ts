0a3ad8c31c80bdb9a8b490ebbbe31d149396fb7a
RakulAgn <rakul0agn@gmail.com>
2022-08-29 10:54:01 +0530

    Function Refactored

--- src/Helper/csv_generate.ts
+++ src/Helper/csv_generate.ts
@@ -3,39 +3,38 @@ import * as fs from 'fs';
 export const InsertDataToCsv = (prodId: string, title: string, desc: string, price: number) => {
 
     console.log(prodId, title, desc, price);
-    
+
 
     const fileExists = fs.existsSync('product.csv');
 
     if (!fileExists) {
-        const CSV_DATA = `PRODUCT_ID,TITLE,DESCRIPTION,PRICE\n${prodId},${title},${desc},${price}`
+        fs.open('product.csv', 'w', (err) => {
+                if (err) throw err;
+                console.log('File is Created Successfully');
+            });
+    }
 
-        return fs.writeFile('product.csv', CSV_DATA, { encoding: "utf8", flag: "a+" }, (err) => {
-            
-            err ? console.log(err) : console.log("File Create Successfully Data Written"); 
-        });
-
-    } else {
-
-        fs.readFile('product.csv', 'utf-8', function (err, data) {
+    fs.readFile('product.csv', 'utf-8', (err, data) => {
 
             var headerIncludes = data.includes('PRODUCT_ID,TITLE,DESCRIPTION,PRICE');
 
-            if (headerIncludes) {
-                const CSV_DATA = `\n${prodId},${title},${desc},${price}`
+            if (!headerIncludes) {
+                const CSV_DATA = `PRODUCT_ID,TITLE,DESCRIPTION,PRICE`;
+                return fs.writeFile('product.csv', CSV_DATA, { encoding: "utf8", flag: "w" }, (err) => {
 
+                    err ? console.log(err) : console.log("Headers Added");
+                });
+            } else {
+                const CSV_DATA = `\n${prodId},${title},${desc},${price}`;
                 return fs.writeFile('product.csv', CSV_DATA, { encoding: "utf8", flag: "a+" }, (err) => {
 
-                    if(err) console.log(err)
+                    if (err) console.log(err);
 
-                    console.log("Row Inserted Successfully"); 
-                    
+                    console.log("Row Inserted Successfully");
+
                 });
             }
 
         })
-
-    }
-
 
 }
